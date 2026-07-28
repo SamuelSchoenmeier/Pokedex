52493a52fd67ecbc1143875b0557584f3e50d4f1
Samuel <samuelschoenmeier@gmail.com>
2026-07-29 01:25:27 +0200

    20 werden aufgerufen

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=20";
+const BASE_URL = "https://pokeapi.co/api/v2/";
 
-async function loadData() {
-    let response = await fetch(BASE_URL + ".json");
+function onloadFunc() {
+    loadData("pokemon?limit=20");
+}
+
+async function loadData(path="") {
+    let response = await fetch(BASE_URL + path + ".json");
     let responseToJson = await response.json();
     console.log(responseToJson);
-    
 }
