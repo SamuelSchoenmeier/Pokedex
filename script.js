c6020289f90cd8326d4f14b313d1e964125bfece
Samuel <samuelschoenmeier@gmail.com>
2026-07-29 16:08:29 +0200

    namen werden auf seite angezeigt

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const BASE_URL = "https://pokeapi.co/api/v2/";
 
-function onloadFunc() {
-    loadData("pokemon?limit=20");
+async function onloadFunc() {
+    let responseToJson = await loadData("pokemon?limit=20");
+    renderContent(responseToJson.results);
 }
 
 async function loadData(path="") {
-    let response = await fetch(BASE_URL + path + ".json");
+    let response = await fetch(BASE_URL + path);
     let responseToJson = await response.json();
     console.log(responseToJson);
+    return responseToJson;
+}
+
+function renderContent(pokemonList) {
+    let contentRef = document.getElementById("content");
+    contentRef.innerHTML = "";
+
+    pokemonList.forEach(pokemon => {
+        contentRef.innerHTML += `
+        <div>${pokemon.name}</div>`
+    });
 }
