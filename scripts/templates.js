36ebca5cc7a8b7bcc6f458e681594ebe613c15b1
Samuel <samuelschoenmeier@gmail.com>
2026-07-31 03:01:03 +0200

    footer logo angepasst, foto wird angezeigt

--- scripts/templates.js
+++ scripts/templates.js
@@ -2,5 +2,6 @@ function dialogTriggerTemplate(currentPokemon) {
     console.log(currentPokemon);
     
     return `<div>${currentPokemon.name}</div>
-            <div>${currentPokemon.id}</div>`;
+            <div>${currentPokemon.id}</div>
+            <img src="${currentPokemon.sprites.front_default}" alt>`;
 }
