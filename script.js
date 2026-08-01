a27b8df4308aa05624400e00e2becf5f777e651e
Samuel <samuelschoenmeier@gmail.com>
2026-08-01 23:29:26 +0200

    main construct dialog opener

--- script.js
+++ script.js
@@ -8,33 +8,46 @@ function init() {
 
 async function onloadFunc() {
     let responseToJson = await loadData("pokemon?limit=20");
+    console.log(responseToJson);
+    
 
     for (let currentPokemon of responseToJson.results) {
         let pokemonDetails = await loadDataFromUrl(currentPokemon.url);
         pokemon.push(pokemonDetails);
+        console.log(pokemonDetails);
+        
     }
 
-    renderContent(pokemon);
+    renderContent();
 }
 
 async function loadData(path="") {
     let response = await fetch(BASE_URL + path);
     let responseToJson = await response.json();
-    console.log(responseToJson);
     return responseToJson;
 }
 
 async function loadDataFromUrl(url) {
     let response = await fetch(url);
     let responseToJson = await response.json();
-    return responseToJson;    
+    return responseToJson;
 }
 
-function renderContent(pokemonList) {
-    let contentRef = document.getElementById("dialog_trigger");
+function renderContent() {
+    let contentRef = document.getElementById("trigger");
     contentRef.innerHTML = "";
 
-    pokemonList.forEach(currentPokemon => {
-        contentRef.innerHTML += dialogTriggerTemplate(currentPokemon)
-    });
+    for (let pokIndex = 0; pokIndex < pokemon.length; pokIndex++) {
+        contentRef.innerHTML += dialogTemplate(pokemon[pokIndex], pokIndex);
+    }
+}
+
+function openDialog(index) {
+    let dialogRef = document.getElementById("dialog");
+    dialogRef.innerHTML = triggerDialogTemplate(pokemon[index]);
+    dialogRef.showModal();
+}
+
+function closeDialog() {
+    document.getElementById("dialog").close();
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,7 +1,17 @@
-function dialogTriggerTemplate(currentPokemon) {
-    console.log(currentPokemon);
-    
-    return `<div>${currentPokemon.name}</div>
-            <div>${currentPokemon.id}</div>
-            <img src="${currentPokemon.sprites.front_default}" alt>`;
+//              TODO: Icons hinzufügen
+function dialogTemplate(currentPokemon) {
+    return  `
+            <div class="dialog-trigger-txt">
+                <p>#${currentPokemon.id}</p>
+                <p>${currentPokemon.name}</p>
+            </div>
+            <img src="${currentPokemon.sprites.front_default}" alt="${currentPokemon.name}">
+            `;
 }
+
+function triggerDialogTemplate(currentPokemon, index) {
+    return  `<div>
+                <div>${currentPokemon.name}</div>
+                <div>${currentPokemon.id}</div>
+            </div>`
+}triggerDialogTemplate
